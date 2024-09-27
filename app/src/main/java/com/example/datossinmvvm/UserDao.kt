3222151993fc66da78e36cce90ba78b66ceb9b93
NinaDIV <168221598+NinaDIV@users.noreply.github.com>
2024-09-26 22:27:02 -0500

    se agrego la funcion de eliminar el ultimo usuario

---
+++ app/src/main/java/com/example/datossinmvvm/UserDao.kt
@@ -0,0 +1,22 @@
+package com.example.datossinmvvm
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM User")
+    suspend fun getAll(): List<User>
+
+    @Insert
+    suspend fun insert(user: User)
+
+    @Query("DELETE FROM User WHERE uid = :uid")
+    suspend fun deleteUser(uid: Int)
+
+
+    @Query("SELECT * FROM User ORDER BY uid DESC LIMIT 1")
+    suspend fun getLastUser(): User?
+}
+
